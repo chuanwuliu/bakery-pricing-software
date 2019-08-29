67a0a549c73c84f130c44628b70c936b3f7692fb
chuanwuliu <dr.liuchuanwu@gmail.com>
2019-08-29 20:09:08 +1000

    Optimise the greedy recursion

--- software/algorithm.py
+++ software/algorithm.py
@@ -3,17 +3,16 @@ This module includes the greedy approximation to bin-packing algorithm. See READ
 """
 
 import numpy as np
-from typing import List
 
 
-def greedy_allocate(remainder: int, v_list: List[int], a_list: List[int] = None, this_bin: int = 0):
+def greedy_allocate(remainder, v_list, a_list=None, pointer=0):
     """
     Allocate remainders to bins (packs).
 
     :param remainder: int, remainder number.
     :param v_list: list, bin volumes (number of units of each pack)
     :param a_list: list, number to each bin
-    :param this_bin: int, recursion pointer.
+    :param pointer: int, recursion pointer.
 
     :return: same as arguments.
     """
@@ -28,48 +27,46 @@ def greedy_allocate(remainder: int, v_list: List[int], a_list: List[int] = None,
     if a_list is None:
         a_list = [0] * n
 
-    a_list[this_bin] = remainder // v_list[this_bin]
-    remainder = remainder % v_list[this_bin]
+    a_list[pointer] = remainder // v_list[pointer]
+    remainder = remainder % v_list[pointer]
 
-    # If the reminder is 0, return the results
-    if remainder == 0:
-        this_bin = 0
-        return remainder, v_list, a_list, this_bin
-
-    # If this bin is the last, return
-    if this_bin == n - 1:
-        this_bin -= 1
-        return remainder, v_list, a_list, this_bin
+    # If this bin is the last, return the result
+    if pointer == n - 1:
+        pointer -= 1
+        return remainder, v_list, a_list, pointer
 
     # If not divisible, try next smaller bin
-    this_bin += 1
-    remainder, v_list, a_list, this_bin = greedy_allocate(remainder, v_list, a_list, this_bin)
+    if remainder > 0:
+        pointer += 1
+        remainder, v_list, a_list, pointer = greedy_allocate(remainder, v_list, a_list, pointer)
+
+    # If the reminder is 0, return the result
+    if remainder == 0:
+        return remainder, v_list, a_list, pointer
 
     # If still not divisible, remove one this bin and allocate to smaller bins
-    if remainder > 0 and a_list[this_bin] > 0:
-        a_list[this_bin] -= 1
-        remainder = remainder + v_list[this_bin]
-        this_bin += 1
-        return greedy_allocate(remainder, v_list, a_list, this_bin)
-    else:
-        return remainder, v_list, a_list, this_bin
+    if a_list[pointer] > 0:
+        a_list[pointer] -= 1
+        remainder = remainder + v_list[pointer]
+        pointer += 1
+        return greedy_allocate(remainder, v_list, a_list, pointer)
 
 
-def exhaustive_allocate(m, v_list, a_list=None, i=0):
+def exhaustive_allocate(remainder, v_list, a_list=None, pointer=0):
     n = len(v_list)
     if a_list is None: a_list = np.zeros(n)
     v_list = np.array(v_list)
-    v = v_list[i]
-    mi = m - (a_list * v_list)[:i].sum()
-    for k in range(0, (int(mi) // v) + 1):
-        a_list[i] = k
-        if (a_list * v_list).sum() == m:
+    v = v_list[pointer]
+    m = remainder - (a_list * v_list)[:pointer].sum()
+    for k in range(0, (int(m) // v) + 1):
+        a_list[pointer] = k
+        if (a_list * v_list).sum() == remainder:
             print(a_list, (a_list * v_list).sum(), "Found")
         else:
             print(a_list, (a_list * v_list).sum())
-        if i < n - 1:
-            exhaustive_allocate(m, v_list, a_list, i + 1)
-    a_list[i] = 0
+        if pointer < n - 1:
+            exhaustive_allocate(remainder, v_list, a_list, pointer + 1)
+    a_list[pointer] = 0
 
 
 if __name__ == '__main__':

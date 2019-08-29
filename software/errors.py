9d0d777e713f895fb39945b3e641f730cbfbacb6
chuanwuliu <dr.liuchuanwu@gmail.com>
2019-08-29 22:47:26 +1000

    Add ExceedCapacity Error to errors

--- software/errors.py
+++ software/errors.py
@@ -1,3 +1,13 @@
+"""
+Bakery pricing errors.
+"""
+
+
 class NoSolution(Exception):
-    def __init__(self, message='No solution'):
+    def __init__(self, message='No solution!'):
+        super().__init__(message)
+
+
+class ExceedCapacity(Exception):
+    def __init__(self, message='Exceed production capacity!'):
         super().__init__(message)

5a5b0df91bf1d4f2499ecd6659212c43a998c18a
chuanwuliu <dr.liuchuanwu@gmail.com>
2019-08-30 23:51:44 +1000

    Add docstring

--- software/process.py
+++ software/process.py
@@ -1,3 +1,7 @@
+"""
+Main processor for processing the order and print the output.
+"""
+
 import os
 import json
 import numpy as np
@@ -9,8 +13,15 @@ projects_path = os.path.join(work_dir, 'products.json')
 
 
 class Processor(object):
-
+    """
+    Processor class
+    """
     def __init__(self, product_table=None):
+        """
+        Processor initialization.
+
+        :param product_table: dict, product table, default None.
+        """
         self._product_table = product_table
 
     @property
@@ -24,27 +35,52 @@ class Processor(object):
         return self._product_table
 
     def pack_table(self, code: str):
+        """
+        Pack data of the given code.
+
+        :param code, str, product code
+        :return dict
+        """
         return self.product_table[code]
 
     def pack_volumes(self, code):
+        """
+        List of volumes/units of units of packs per code.
+
+        :param code: str, product code
+        :return: list
+        """
         v_list = [pack['units'] for pack in self.pack_table(code)]
         return v_list
 
     def pack_price(self, code: str, units: int):
+        """
+        Price of pack with given code and units.
+
+        :param code: str, product code
+        :param units: int, number of unit
+        :return: int, price
+        """
         for pack in self.pack_table(code):
             if pack['units'] == units:
                 return pack['price']
         raise ProductError('{} {} <== Pack price not found!'.format(units, code))
 
     def capacity(self, code: str):
+        """
+        Maximum production capacity of the bakery.
+
+        :param code: str, product code
+        :return: int
+        """
         return self.product_table['capacity'][code]
 
     def order_data(self, path: str):
         """
-        path to the oder file. see tests for example order files
+        Input order data read from the file path. the input file must in JSON format.
 
         :param path: str, path to order file
-        :return: pd.DataFrame, order data
+        :return: dict
         """
         with open(path) as file:
             order = json.load(file)
@@ -59,8 +95,14 @@ class Processor(object):
         return order
 
     def process_order(self, path: str):
+        """
+        Process the order from the file path. The input file must be in JSON format.
+
+        :param path: str, path to the input order.
+        """
         order = self.order_data(path)
         for code, quantity in order.items():
+            # Allocate packs to order using algorithms
             v_list = self.pack_volumes(code)
             try:
                 remainder, v_list, a_list, pointer = allocate(quantity, v_list)
@@ -72,6 +114,7 @@ class Processor(object):
                     raise NoSolution()
                 a_list = a_list_opt[0]
 
+            # Price and output
             price_list = [self.pack_price(code, units) for units in v_list]
             total_price = np.dot(a_list, price_list).sum()
             print("{} {} $ {}".format(quantity, code, total_price.round(2)))
